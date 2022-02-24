077178429e782a631add25cbdabc9bbae5b9cace
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2022-02-24 20:17:39 +0100

    created buildSrc which will combine common dependencies and behaviour

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,7 @@
 plugins {
     id("org.springframework.boot") version "2.6.3"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("com.github.ben-manes.versions")
 
-    kotlin("jvm")
     kotlin("plugin.spring") version "1.6.20-M1"
 }
 
