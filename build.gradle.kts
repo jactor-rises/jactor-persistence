79c37f4a0867d0063633cd4ab0d08533f11c9481
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2022-02-25 14:48:02 +0100

    created version catalog in gradle
    - removed unused dependency: apache.commons:commons-lang3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,16 @@
+group = "com.github.jactor-rises"
+version = "2.0.x-SNAPSHOT"
+description = "jactor::persistence"
+
 dependencies {
     // spring-boot
     implementation("org.springframework.boot:spring-boot-starter-aop")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
-    // --- misc dependencies ---
-    implementation("org.apache.commons:commons-lang3:3.12.0")
-
     // database
-    runtimeOnly("org.flywaydb:flyway-core:8.5.1")
-    runtimeOnly("com.h2database:h2:2.1.210")
+    runtimeOnly(libs.h2)
+    runtimeOnly(libs.flyway)
 
     // swagger
-    implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
+    implementation(libs.swagger)
 }
-
-group = "com.github.jactor-rises"
-version = "2.0.x-SNAPSHOT"
-description = "jactor::persistence"
