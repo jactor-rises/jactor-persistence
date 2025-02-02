ff3c7d3284a3bf9ff066cc1d48a8f4942e735bbf
Tor Egil Jacobsen <jactor-rises@users.noreply.github.com>
2025-02-02 11:48:04 +0100

    bump spring-boot, 3.3.5 -> 3.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ version = "2.0.x-SNAPSHOT"
 description = "jactor::persistence"
 
 plugins {
-    id("org.springframework.boot") version "3.3.5"
+    id("org.springframework.boot") version "3.4.2"
 }
 
 dependencies {
