303643faa23e39abba37eb5980ce5916a98e7342
Tor Egil Jacobsen <tor.egil.jacobsen@gmail.com>
2025-03-01 21:09:42 +0100

    StackTraceElementExtensions.kt & ThrowableExtensions.kt
    - also new-semver.main.kts
    - also tag-persistence.yaml
    - also tag-web.yaml

--- src/main/kotlin/com/github/jactor/persistence/api/exception/ExceptionHandler.kt
+++ src/main/kotlin/com/github/jactor/persistence/api/exception/ExceptionHandler.kt
@@ -6,13 +6,13 @@ import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
-import com.github.jactor.persistence.util.whenFalse
-import com.github.jactor.persistence.util.whenTrue
-import com.github.jactor.shared.filEllerKlassenavn
-import com.github.jactor.shared.kodelinjeEllerMetode
+import com.github.jactor.shared.exceptionMessageMedCause
+import com.github.jactor.shared.finnFeiledeLinjer
+import io.github.oshai.kotlinlogging.KotlinLogging
+
+private val kLogger = KotlinLogging.logger {}
 
 @ControllerAdvice
 class ExceptionHandler : ResponseEntityExceptionHandler() {
@@ -26,71 +26,20 @@ class ExceptionHandler : ResponseEntityExceptionHandler() {
         handleException(e, request, HttpStatus.BAD_REQUEST)
 
     private fun handleException(
-        e: RuntimeException,
-        request: WebRequest,
+        runtimeException: RuntimeException,
+        webRequest: WebRequest,
         httpStatus: HttpStatus,
-    ): ResponseEntity<Any>? {
-        val message = exceptionMessageMedCause(e)
-
+    ): ResponseEntity<Any>? = runtimeException.exceptionMessageMedCause().let { message ->
         val headers = HttpHeaders().apply {
             contentType = MediaType.APPLICATION_JSON
             put(HttpHeaders.WARNING, listOf(message))
         }
 
-        return handleExceptionInternal(e, ErrorResponse(message), headers, httpStatus, request)
-    }
-
-    companion object {
-        private fun exceptionMessage(t: Throwable) = if (t is HttpClientErrorException) {
-            "Internal client, ${t.statusCode}: ${t.message}"
-        } else {
-            "${t::class.simpleName}: ${t.message}"
+        kLogger.error(runtimeException) {
+            "$message: ${runtimeException.finnFeiledeLinjer().joinToString { ", " }}"
         }
 
-        private fun findRootCauseMessage(cause: Throwable?): String? {
-            var rootCause: Throwable? = cause
-
-            while (rootCause?.cause != null) {
-                rootCause = rootCause.cause
-            }
-
-            return rootCause?.let { "${it.javaClass.simpleName}: ${it.message}" }
-        }
-
-        fun exceptionMessageMedCause(throwable: Throwable): String {
-            val exceptionMessage = exceptionMessage(throwable)
-            val causeMessage = throwable.cause?.let { " - caused by ${findRootCauseMessage(it)}" } ?: ""
-
-            return "$exceptionMessage$causeMessage".take(10000)
-        }
-
-        fun finnFeiledeLinjer(throwable: Throwable): List<String> {
-            val linjer = mutableListOf<String>()
-            var cause: Throwable? = throwable
-
-            while (cause != null) {
-                val stacks = cause.stackTrace
-                val indent: String = (cause == throwable).whenFalse { "..." } ?: ""
-
-                for (index in stacks.indices) {
-                    val frame = stacks[index]
-                    val isInternal = frame.className.startsWith("com.github.jactor")
-
-                    if (isInternal || index == 0) {
-                        linjer.add(
-                            indent +
-                                "${isInternal.whenTrue { "intern" } ?: "ekstern"}: " +
-                                "${frame.filEllerKlassenavn()} " +
-                                "(${frame.kodelinjeEllerMetode()})",
-                        )
-                    }
-                }
-
-                cause = cause.cause
-            }
-
-            return linjer
-        }
+        handleExceptionInternal(runtimeException, ErrorResponse(message), headers, httpStatus, webRequest)
     }
 
     @JvmRecord
