e2364d32182a91a6accd2bb2441a11acc9638b0e
Tor Egil Jacobsen <Toregil.jacobsen@skatteetaten.no>
2025-11-08 13:51:20 +0100

    #115: new version of exposed resolved issue

--- src/test/kotlin/com/github/jactor/persistence/person/PersonServiceTest.kt
+++ src/test/kotlin/com/github/jactor/persistence/person/PersonServiceTest.kt
@@ -25,7 +25,6 @@ internal class PersonServiceTest @Autowired constructor(
     }
 
     @Test
-    @Disabled("Disabled because of transaction management on CI???, but not locally")
     fun `should find Person by id`() = runTest {
         val personDao = personRepository.save(initPerson().toPersonDao())
 
